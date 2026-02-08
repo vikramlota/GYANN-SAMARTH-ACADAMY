49a6bd11f3af5be9d416e9f1b152e0a245e3d6e2
vikramlota <viccckkylota@gmail.com>
2026-02-08 15:13:20 +0530

    admin register

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -5,9 +5,16 @@ dotenv.config({ path: './.env' });
 const connectDB = require('./db/index.js');
 const { app } = require('./app.js');
 const serverless = require('serverless-http');
+const seedAdmin = require('./utils/adminSeeder.js');
 
 // Connect once (reused across lambda invocations where possible)
-connectDB().catch((err) => {
+connectDB()
+  .then(() => {
+     // This runs immediately after DB connects
+     console.log("DB Connected. Checking for Admin seed...");
+     seedAdmin(); 
+  })
+  .catch((err) => {
     console.log('MONGO DB connection failed !!!', err);
 });
 
