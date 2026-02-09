8cdf02990e92ac7a7ec0355e7be3d3d14938ad67
vikramlota <viccckkylota@gmail.com>
2026-02-09 20:08:51 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const connectDB = require('./db/index.js');
 
 const app = express()
 
