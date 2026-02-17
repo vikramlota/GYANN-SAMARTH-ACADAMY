4621d5b34bc53d579fe225e650899aa02d622009
vikramlota <viccckkylota@gmail.com>
2026-02-17 10:09:39 +0530

    Update update.routes.js

--- Backend/src/routes/update.routes.js
+++ Backend/src/routes/update.routes.js
@@ -8,8 +8,4 @@ const upload = require('../middlewares/upload.middleware.js');
 router.route('/').get(getUpdates).post(protect, upload.single('image'), createUpdate);
 router.route('/:id').get(getUpdateById).delete(protect, deleteUpdate);
 
-// Current Affairs Routes
-router.route('/current-affairs/all').get(getCurrentAffairs);
-router.route('/current-affairs/create').post(protect, upload.single('image'), createCurrentAffair);
-
 module.exports = router;
