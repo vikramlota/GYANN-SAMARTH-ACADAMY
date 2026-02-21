99b4c910adb30f3d268e43fe33d8ad98ea24d069
vikramlota <viccckkylota@gmail.com>
2026-02-21 15:19:01 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom'; // Use Link for internal routing
-import { FaFacebookF, FaInstagram, FaWhatsapp, FaChevronRight, FaPhoneAlt, FaEnvelope, FaMapMarkerAlt, FaGooglePlay } from 'react-icons/fa';
+import { FaFacebookF, FaInstagram, FaWhatsapp, FaChevronRight, FaPhoneAlt, FaEnvelope, FaMapMarkerAlt, FaGooglePlay, FaAppStore } from 'react-icons/fa';
 import { useScrollReveal } from '../hooks';
 import api from '../utils/api'; // Make sure this path is correct for your project
 
@@ -43,6 +43,8 @@ const Footer = () => {
                 <a href="https://www.instagram.com/gyanm.samarth.academy?igsh=MTB0NnI5dTliNDlicA%3D%3D" className="w-8 h-8 flex items-center justify-center bg-white text-brand-red rounded-full hover:bg-brand-orange hover:text-white transition transform hover:scale-110 shadow-md"><FaInstagram /></a>
                 <a href="https://wa.me/919041973105" className="w-8 h-8 flex items-center justify-center bg-white text-brand-red rounded-full hover:bg-brand-orange hover:text-white transition transform hover:scale-110 shadow-md" target="_blank" rel="noreferrer"><FaWhatsapp /></a>
                 <a href="https://play.google.com/store/apps/details?id=co.diy4.ptqkn&pcampaignid=web_share" className="w-8 h-8 flex items-center justify-center bg-white text-brand-red rounded-full hover:bg-brand-orange hover:text-white transition transform hover:scale-110 shadow-md" target="_blank" rel="noreferrer"><FaGooglePlay /></a>
+                <a href="https://apps.apple.com/in/app/classplus/id1324522260" className="w-8 h-8 flex items-center justify-center bg-white text-brand-red rounded-full hover:bg-brand-orange hover:text-white transition transform hover:scale-110 shadow-md" target="_blank" rel="noreferrer"><FaAppStore /></a>
+                <h6 className="text-xs text-white mt-2">ClassPlus Code for IOS: </h6>
                 
             </div>
           </div>
