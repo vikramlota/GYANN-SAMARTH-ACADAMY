fcd9de8fdbddc1fbdaf9eabfc98a26e9c9bbc897
vikramlota <viccckkylota@gmail.com>
2026-02-22 16:36:29 +0530

    Update ManageUpdates.jsx

--- frontend/src/admin/ManageUpdates.jsx
+++ frontend/src/admin/ManageUpdates.jsx
@@ -89,7 +89,7 @@ const ManageUpdates = () => {
       
       <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
         {/* FORM */}
-        <div className="md:col-span-3 bg-white p-6 rounded-xl shadow border h-fit">
+        <div className="lg:col-span-3">
           <form onSubmit={handleSubmit} className="space-y-4">
              <input name="title" value={formData.title} onChange={(e)=>setFormData({...formData, title:e.target.value})} placeholder="Title (e.g. SBI PO Out)" required className="w-full border p-2 rounded"/>
              
@@ -133,7 +133,7 @@ const ManageUpdates = () => {
         </div>
 
         {/* LIST */}
-          <div className="md:col-span-2 space-y-3">
+          <div className="lg:col-span-2">
             {updates.map(item => (
              <div key={item._id} className="bg-white p-4 rounded-xl shadow border flex justify-between items-center">
                <div>
